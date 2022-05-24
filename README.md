931ea077066cfef60a5312d56fd268cbbcaf0bca
wenjiangf <f_wenjiang@163.com>
2022-05-24 15:06:04 +0800

    添加readme，使用删除.env，使.env.local本地配置文件

--- README.md
+++ README.md
@@ -1,24 +1,19 @@
-# cycler
+### 效果展示
 
-## Project setup
-```
-npm install
-```
+![app](https://img-blog.csdnimg.cn/93782f29a13e421292bd35cf1c6f0382.png)
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
+![搜索](https://img-blog.csdnimg.cn/f203355cd67944179b3d0ce0b53a21e5.png)
 
-### Compiles and minifies for production
-```
-npm run build
-```
+![插件市场](https://img-blog.csdnimg.cn/408296d18d2a4276820823cbba80207f.png)
 
-### Lints and fixes files
-```
-npm run lint
-```
+![我的插件](https://img-blog.csdnimg.cn/d1f7dbea498747308bfee8b75608a09d.png)
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+![便好设置](https://img-blog.csdnimg.cn/e3c51dbd9192480a8404cc32b9fa29cd.png)
+
+### 完成情况
+
+- [x] 搜索插件
+- [x] 加载插件
+- [x] 安装插件
+- [x] 卸载插件
+- [ ] 便好设置
--- src/renderer/components/Result.vue
+++ src/renderer/components/Result.vue
@@ -1,30 +1,40 @@
 <template>
   <div class="result_options" v-show="!currentPlugin?.name">
     <a-list>
-      <a-list-item v-for="item in options" :key="item.label" @click="emit('selectPlugin', Object.assign(item.plugin, {cmd: item.label}))">
+      <a-list-item
+        v-for="item in options"
+        :key="item.label"
+        @click="
+          emit('selectPlugin', Object.assign(item.plugin, { cmd: item.label }))
+        "
+      >
         {{ item.label }}
       </a-list-item>
     </a-list>
   </div>
 </template>
 <script setup lang="ts">
-import { defineProps, defineEmits, withDefaults } from 'vue'
-import { SearchOptions, AdapterInfo } from '@/types/type'
+import { defineProps, defineEmits, withDefaults } from "vue";
+import { SearchOptions, AdapterInfo } from "@/types/type";
 
 interface ResultProps {
-  options: SearchOptions[],
-  currentPlugin: AdapterInfo|null
+  options: SearchOptions[];
+  currentPlugin: AdapterInfo | null;
 }
 
-const emit = defineEmits(['selectPlugin'])
+const emit = defineEmits(["selectPlugin"]);
 const props = withDefaults(defineProps<ResultProps>(), {
-  options: () =>  [],
-  currentPlugin: null
-})
+  options: () => [],
+  currentPlugin: null,
+});
 </script>
 
 <style lang="less">
 .result_options {
   background-color: rgb(245, 245, 245);
+  .ant-list-item {
+    padding-left: 11px;
+    padding-right: 11px;
+  }
 }
 </style>
